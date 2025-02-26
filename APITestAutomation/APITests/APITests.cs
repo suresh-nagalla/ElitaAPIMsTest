4496d57d6b0e50d211fd60227a77a89394a4522a
suresh-nagalla <suri.nagalla@gmail.com>
2025-02-26 14:13:50 +0530

    Create APITestAutomation/APITests/APITests.cs

---
+++ APITestAutomation/APITests/APITests.cs
@@ -0,0 +1,33 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using APITestAutomation.APIutility;
+using APITestAutomation.Models;
+using System.Net;
+
+namespace APITestAutomation.APITests
+{
+    [TestClass]
+    public class APITests
+    {
+        private const string BaseUrl = "https://dummyjson.com";
+        private const string LoginEndpoint = "/auth/login";
+
+        [TestMethod]
+        public void API_InvalidLogin_ShouldReturnErrorMessage()
+        {
+            // Arrange
+            var apiUtility = new APIUtility(BaseUrl);
+            var loginRequest = new LoginRequest
+            {
+                Username = "user",
+                Password = "invalidCreds"
+            };
+
+            // Act
+            var response = apiUtility.Post<LoginResponse, LoginRequest>(LoginEndpoint, loginRequest);
+
+            // Assert
+            Assert.IsNotNull(response, "Response should not be null");
+            Assert.AreEqual("Username and password required", response.Message, "The error message is not as expected.");
+        }
+    }
+}
